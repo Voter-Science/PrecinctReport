3e0f9f1157eafeb9ac7b4d3841ada11868c5c160
Voter-Science <Info@Voter-Science.com>
2018-06-13 22:29:52 -0700

    Add

--- src/pluginmain.ts
+++ src/pluginmain.ts
@@ -11,12 +11,16 @@ import * as common from 'trc-httpshim/common'
 
 import * as core from 'trc-core/core'
 
-import * as trcSheet from 'trc-sheet/sheet' 
+import * as trcSheet from 'trc-sheet/sheet'
+import {ISheetContents } from 'trc-sheet/sheetContents'
 import * as trcSheetEx from 'trc-sheet/sheetEx'
 
-import * as gps from 'trc-web/gps'
 import * as plugin from 'trc-web/plugin'
 import * as trchtml from 'trc-web/html'
+
+import * as bcl from 'trc-analyze/collections'
+import * as analyze from 'trc-analyze/core'
+import * as precints from 'trc-analyze/precinct'
 
 
 // Installed via:
@@ -26,57 +30,45 @@ declare var $: JQueryStatic;
 
 // Provide easy error handle for reporting errors from promises.  Usage:
 //   p.catch(showError);
-declare var showError : (error:any) => void; // error handler defined in index.html
+declare var showError: (error: any) => void; // error handler defined in index.html
 
 export class MyPlugin {
     private _sheet: trcSheet.SheetClient;
-    private _pluginClient : plugin.PluginClient;
-    private _gps : common.IGeoPointProvider;
+    private _pluginClient: plugin.PluginClient;
 
     public static BrowserEntryAsync(
         auth: plugin.IStart,
-        opts : plugin.IPluginOptions
-    ) : Promise<MyPlugin> {
-        
-        // You can set gpsTracker null if you don't need GPS. 
-        var gpsTracker = new gps.GpsTracker(); // Only works in browser
-        var pluginClient = new plugin.PluginClient(auth,opts, gpsTracker);
+        opts: plugin.IPluginOptions
+    ): Promise<MyPlugin> {
+
+        var pluginClient = new plugin.PluginClient(auth, opts);
 
         // Do any IO here...
-        
-        var throwError =false; // $$$ remove this
-        
+
+        var throwError = false; // $$$ remove this
+
         var plugin2 = new MyPlugin(pluginClient);
-        plugin2._gps = gpsTracker;
-        return plugin2.InitAsync().then( () => {
+        return plugin2.InitAsync().then(() => {
             if (throwError) {
                 throw "some error";
             }
-            
-            gpsTracker.start((loc) => plugin2.OnGpsChanged(loc)); // ignore callback
-            return plugin2;                        
+
+            return plugin2;
         });
     }
 
-    private OnGpsChanged(loc : gps.IGeoPoint) : void {
-        // Notification that the GPS position has change. 
-        // Use this if you have a map view showing the user's "current location"
-        var msg = "(Lat: " + loc.Lat + ", Long:" + loc.Long + ")";
-        $("#locInfo").text(msg);
-    }
-
     // Expose constructor directly for tests. They can pass in mock versions. 
-    public constructor(p : plugin.PluginClient) {
+    public constructor(p: plugin.PluginClient) {
         this._sheet = new trcSheet.SheetClient(p.HttpClient, p.SheetId);
     }
-    
+
 
     // Make initial network calls to setup the plugin. 
     // Need this as a separate call from the ctor since ctors aren't async. 
-    private InitAsync() : Promise<void> {
-        return this._sheet.getInfoAsync().then( info  => {
+    private InitAsync(): Promise<void> {
+        return this._sheet.getInfoAsync().then(info => {
             this.updateInfo(info);
-        });     
+        });
     }
 
     // Display sheet info on HTML page
@@ -87,33 +79,82 @@ export class MyPlugin {
         $("#RowCount").text(info.CountRecords);
 
         $("#LastRefreshed").text(new Date().toLocaleString());
-    }
 
-    // Example of a helper function.
-    public doubleit(val: number): number {
-        return val * 2;
-    }
-
-    // Demonstrate receiving UI handlers 
-    public onClickRefresh(): void {
-        this.InitAsync().        
-            catch(showError);
-    }
-
-
-    // downloading all contents and rendering them to HTML can take some time. 
-    public onGetSheetContents(): void {
         trchtml.Loading("contents");
         //$("#contents").empty();
         //$("#contents").text("Loading...");
 
-        trcSheetEx.SheetEx.InitAsync(this._sheet, this._gps).then((sheetEx)=>
-        {
-            return this._sheet.getSheetContentsAsync().then((contents) => {
-                var render = new trchtml.SheetControl("contents", sheetEx);
-                // could set other options on render() here
-                render.render();
-            }).catch(showError);
-        });        
+        var b = new precints.BuildPrecinctReport();
+
+
+        this._sheet.getSheetContentsAsync().then((contents) => {
+
+            var p = b.build(contents);
+
+            var precinctContents = precints.BuildPrecinctReport.convert(p);
+
+
+            var e = document.getElementById("download");
+            trchtml.DownloadHelper.appendDownloadCsvButton(e, () => precinctContents);
+
+
+            var render = new trchtml.RenderSheet("contents", precinctContents);
+            // could set other options on render() here
+            render.render();
+
+            // compute totals and render
+            var totals : ISheetContents = { };
+            var names : string[] = [] ;
+            var vals : string[] = [];
+            totals["Names"] = names;
+            totals["Value"] = vals;
+
+            names.push("Total Count");
+            vals.push(this.sum(precinctContents, "count"));
+
+            names.push("Total Households");
+            vals.push(this.sum(precinctContents, "HouseholdCount"));
+
+            names.push("Total GOP");
+            vals.push(this.sum(precinctContents, "GOPCount"));
+
+            names.push("Total Dem");
+            vals.push(this.sum(precinctContents, "DEMCount"));
+
+            names.push("Total Contacts");
+            vals.push(this.sum(precinctContents, "ContactCount"));
+
+            names.push("Total Contacted Households");
+            vals.push(this.sum(precinctContents, "ContactHouseholdCount"));
+
+            names.push("Total Targets");
+            vals.push(this.sum(precinctContents, "Targets"));
+
+            var render = new trchtml.RenderSheet("totals", totals);
+            // could set other options on render() here
+            render.render();
+
+            
+        }).catch(showError);
+    }
+
+
+    private sum(data : ISheetContents, columnName : string ) : string {
+        var col = data[columnName];
+        if (!col) {
+            return "na";
+        }
+
+        var total = 0;
+        for(var i in col) {
+            var value = col[i];
+
+            var x = parseFloat(value);
+            if (x != NaN)  {
+                total += x;
+            }
+        }
+
+        return total.toLocaleString();
     }
 }
